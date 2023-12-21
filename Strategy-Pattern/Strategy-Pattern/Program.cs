f03f924ad3c9d310d463a976cb38fd5950585ba9
Yves <yvesxianwu@gamil.com>
2023-12-21 22:25:27 +0800

    update strategy pattern

---
+++ Strategy-Pattern/Strategy-Pattern/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.display();
+            mallard.performFly();
+            mallard.performQuack();
+            Console.ReadKey();
+
+            Duck model = new ModelDuck();
+            model.display();
+            model.performFly();
+            model.performQuack();
+            Console.ReadKey();
+        }
+    }
+    /* parent class : Duck */
+    public abstract class Duck
+    {
+        public FlyBehavior flyBehavior;
+        public QuackBehavior quackBehavior;
+        public Duck()
+        {
+            
+        }
+
+        public abstract void display();
+
+        public void performFly()
+        {
+            flyBehavior.fly();
+        }
+
+        public void performQuack()
+        {
+            quackBehavior.quack();
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("All ducks float, even decoys!");
+        }
+    }
+
+    /* sub-class: ModelDuck */
+    public class ModelDuck : Duck
+    {
+        public ModelDuck()
+        {
+            flyBehavior = new FlyNoWay();
+            quackBehavior = new Quack();
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("I'm a model duck");
+        }
+    }
+
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            quackBehavior = new Quack();
+            flyBehavior = new FlyWithWings();
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("I'm a real Mallard duck");
+        }
+    }
+
+    /* fly behavior */
+    public interface FlyBehavior
+    {
+        public void fly();
+    }
+
+    public class FlyWithWings : FlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I'm flying!");
+        }
+    }
+
+    public class FlyNoWay : FlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("I'm can't fly");
+        }
+    }
+
+    /* quack behavior */
+    public interface QuackBehavior
+    {
+        public void quack();
+    }
+
+    public class Quack : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("Quack!");
+        }
+    }
+
+    public class MuteQuack : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("Silence");
+        }
+    }
+
+    public class Squeak : QuackBehavior
+    {
+        public void quack()
+        {
+            Console.WriteLine("Squeak!");
+        }
+    }
+}
